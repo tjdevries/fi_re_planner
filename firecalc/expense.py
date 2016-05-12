bbc78f21c307b25aa9309c7a5a74fdb4959cf88b
TJ DeVries <devries.timothyj@gmail.com>
2016-05-11 21:12:56 -0400

    Add expense and amount

--- firecalc/expense.py
+++ firecalc/expense.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+class Amount(Object):
+    def __init__(self, style, period, amount):
+
 class Expense(Object):
-    def __init__(self, amount, 
+    def __init__(self, amount: Amount):
+        self.amount = amount
